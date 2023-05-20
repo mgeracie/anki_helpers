3ee9e65c308e43b47f03ee329beef96ed3e5cf2b
Michael Geracie <michael.geracie@gmail.com>
2023-05-20 13:43:41 -0400

    can now colorize character strings

--- test.py
+++ test.py
@@ -19,7 +19,11 @@ def get_tone(syl):
 def is_single_syl(s):
     if s == " ":
         return True
-    if len(re.findall("\s", s)) > 0:
+    if len(re.findall(r"\s", s)) > 0:
+        return False
+    if len(re.findall(r"[\u4e00-\u9fff]", s)) > 1:
+        return False
+    if len(re.findall(r"[\u4e00-\u9fff]", s)) == 1 and len(s) > 1:
         return False
     s_pin = (s in pin_all_syl)
     s_contains_pin = (sum([len(re.findall(syl, s)) for syl in pin_all_syl]) == 0)
@@ -30,12 +34,12 @@ def is_single_char(s):
         return False
     return len(re.findall(r'[\u4e00-\u9fff]+', s)) == 1
 
-def break_simple(s: str, is_char = False):
+def break_simple(s, is_char = False):
     s_split = ""
     # return if already a single syllable
     if (is_single_syl(s) or s == " "):
         return [s]
-    
+
     # break up by whitespace if you can
     if s != " ":
         s_split = re.findall(r'\S+', s)
@@ -43,12 +47,11 @@ def break_simple(s: str, is_char = False):
         if len(s_split) > 1:
             return s_split
     
-    # split up by characters
+    # break up by char
     if is_char:
-        s_split = re.split(r'([\u4e00-\u9fff]+)', s)
-        s_split = [t.strip() for t in re.split(f"({syl})", s) if t.strip() != ""]
+        s_split = [t.strip() for t in re.split(r"([\u4e00-\u9fff])", s)  if t.strip() != ""]
         s_split = list(collapse(s_split))
-    # otherwise, split up by pinyin syllables
+    # break up by pinyin
     else:
         for syl in pin_all_syl:
             if syl in s:
@@ -58,15 +61,29 @@ def break_simple(s: str, is_char = False):
 
     return s_split
 
-def break_pinyin(s):
+def break_string_full(s, is_char = False):
     s_list = [s]
     is_broken = False
     while not is_broken: 
-        s_list = [break_simple(s) for s in collapse(s_list)]
+        s_list = [break_simple(s, is_char) for s in collapse(s_list)]
+        while (len(s_list) == 1) and (type(s_list[0]) == list):
+            s_list = s_list[0]
         s_list = list(collapse(s_list))
         is_broken = all([is_single_syl(s) for s in s_list])
-    s_broken = [get_tone(s) for s in s_list]
+    s_list = [get_tone(s) for s in s_list]
+    return s_list
 
+def break_char_string_full(s_char, s_pin):
+    s_pin_list = break_string_full(s_pin)
+    s_char_list = break_string_full(s_char, True)
+    s_char_list = [(s_char_list[i][0], s_pin_list[i][1]) for i in range(len(s_char_list))]
+    return s_char_list
+
+def break_string(s, s_char = ""):
+    if s_char != "":
+        s_broken = break_char_string_full(s_char, s)
+    else:
+        s_broken = break_string_full(s)
     output = []
     in_process_syl = ("", 5)
     for i in range(0, len(s_broken)):
@@ -90,7 +107,7 @@ def stylize_pinyin(s):
     span_4 = "<span class = 'tone4'>"
     span_5 = "<span class = 'tone5'>"
     end_span = "</span>"
-    s_list = break_pinyin(s)
+    s_list = break_string(s)
     
     s_out = "".join([span_1 + s[0] + end_span if s[1] == 1
                         else span_2 + s[0] + end_span if s[1] == 2
@@ -104,8 +121,11 @@ def stylize_pinyin(s):
 s1 = "hello there gōngyuán,  44 běi zhuáng - qiánzhuáng3"
 s2 = "hello there gōngyuán,  44 běi zhuáng - qiánzhuáng"
 
+c = "公园"
+d = "园3"
+
 c1 = "hello there 公园, 44 北 妆 - 前妆3"
-c2 = "园"
+c2 = "hello there 公园, 44 北 妆 - 前妆"
 
 def break_characters(s):
     # break up by whitespace if you can
